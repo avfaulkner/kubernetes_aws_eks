78605ea5b7fc57f0f4250b15507e605239c5f0ff
afaulkner <avfaulkner@gmail.com>
2022-03-03 12:51:38 -0500

    rm unneeded vars

--- variables.tf
+++ variables.tf
@@ -6,22 +6,6 @@ variable "owner" {
   description = "Owner of resources"
 }
 
-variable "private_subnet_block_1" {
-  description = "Private subnet"
-}
-
-variable "private_subnet_block_2" {
-  description = "Private subnet"
-}
-
-variable "public_subnet_block_1" {
-  description = "Public subnet"
-}
-
-variable "public_subnet_block_2" {
-  description = "Public subnet"
-}
-
 variable "region" {
   description = "Region in which the cluster is placed"
 }
